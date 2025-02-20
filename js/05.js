367a0181402aee0982db63222d026152e1220261
Daniel_Jimenez <djimeneza7b2276@gmail.com>
2025-02-20 16:30:28 -0600

    se añaden las funciones con math

---
+++ js/05.js
@@ -0,0 +1,10 @@
+//numeros 
+
+const nombre = "daniel";
+const numero1 = 100;
+const numero2 = 200;
+const numero3 = 300.23;
+const numero4 = -20;
+console.log(nombre);
+console.log(numero2);
+console.log(numero1 - numero2);
---
+++ js/06.js
@@ -0,0 +1,17 @@
+//objeto math
+
+let resultado;
+
+resultado = Math.PI;
+resultado = Math.round(2.4); //redondea
+resultado = Math.ceil(2.1); //redondea hacia arriba
+resultado = Math.floor(2.9); //redondea hacia abajo
+resultado = Math.sqrt(144); //raiz cuadrada
+resultado = Math.abs(-200); //valor absoluto
+resultado = Math.min(3,5,1,12,4); //minimo
+resultado = Math.max(3,5,1,12,4); //maximo
+resultado = Math.random(); //numero aleatorio
+resultado = Math.floor(Math.random() * 30); //numero aleatorio entre 0 y 30
+
+
+console.log(resultado); 
